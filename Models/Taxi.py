9fdcaf62d296c8926782e9357d524d464c60ef4a
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2024-03-28 15:04:02 -0500

    adds data payload for the Cab class

--- Models/Taxi.py
+++ Models/Taxi.py
@@ -1,7 +1,17 @@
-class Taxi:
+class Cab:
     def __init__(self, placa: str, capacidadTanque: int):
         self.placa = placa
         self.capacidadTanque = capacidadTanque
+
+    def load_cabs(data: dict):
+        cabs = []
+        for cab_data in data["taxis"]:
+            cab = Cab(
+                cab_data["placa"],
+                cab_data["capacidad_tanque"],
+            )
+            cabs.append(cab)
+        return cabs
 
     def obtenerPlaca(self):
         return self.placa
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Models.Node import Node
+from Models.Taxi import Cab
 from func.data_loading import extract_data
 
 
@@ -10,8 +11,16 @@ def main():
     # Array of all nodes
     nodes = Node.load_nodes(data)
 
+    # Array of all cabs
+    cabs = Cab.load_cabs(data)
+
+    print("Nodes data")
     for node in nodes:
         print(node.nombre)
+
+    print("Cabs data")
+    for cab in cabs:
+        print(cab.placa)
 
 
 if __name__ == "__main__":
