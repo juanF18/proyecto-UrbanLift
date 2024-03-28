90a8fc9abd96edb5c83ffe94599362782a993f5b
camian0 <74461803+camian0@users.noreply.github.com>
2024-03-28 14:31:10 -0500

    Create nodes

---
+++ Models/Node.py
@@ -0,0 +1,53 @@
+class Nodes:
+    def __init__(
+        self,
+        nombre: str,
+        carrera: str,
+        calle: str,
+        semaforo: bool,
+        puntoInteres: bool,
+        conexiones: list,
+    ):
+        self.nombre = nombre
+        self.carrera = carrera
+        self.calle = calle
+        self.semaforo = semaforo
+        self.puntoInteres = puntoInteres
+        self.conexiones = conexiones
+
+    # Method to set new value to node name
+    def setNombre(self, nuevoNombre: str):
+        self.nombre = nuevoNombre
+
+    # Method to get attribute name of node
+    def getNombre(self) -> str:
+        return self.nombre
+
+    # Method to set attribute carrera of node
+    def setCarrera(self, nuevaCarrera: str):
+        self.carrera = nuevaCarrera
+
+    # Method to get attribute carrera of node
+    def getCarrera(self):
+        return self.carrera
+
+    # Method to set attribute calle of node
+    def setCalle(self, nuevaCalle: str):
+        self.calle = nuevaCalle
+
+    # Method to get attribute calle of node
+    def getCalle(self):
+        return self.calle
+
+    # Method to set attribute semaforo of node
+    def setSemaforo(self, nuevoEstado: bool):
+        self.semaforo = nuevoEstado
+
+    # Method to get of attribute semaforo
+    def get_semaforo(self):
+        return self.semaforo
+
+    # Method to set new state of punto interes attribute
+    def setPuntoInteres(self, nuevoEstado: bool):
+        if nuevoEstado != None:
+            self.puntoInteres = nuevoEstado
