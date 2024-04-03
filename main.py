2f3e1a4f275a5a274584225ae3035f71aab1698d
Camilo <camian6@outlook.com>
2024-04-02 20:13:05 -0500

    Route class was called into main method
    The route clas was called and deleted some code

--- main.py
+++ main.py
@@ -1,26 +1,22 @@
 from Models.Node import Node
 from Models.Cab import Cab
 from func.data_loading import extract_data
+from Models.Routes import Routes
 
 
 def main():
-    path_data = "./data/datos.json"
+    path_data = "./data/datos2.json"
 
     data = extract_data(path_data)
 
-    # Array of all nodes
+    # dictionary of all nodes
     nodes = Node.load_nodes(data)
 
     # Array of all cabs
     cabs = Cab.load_cabs(data)
 
-    print("Nodes data")
-    for node in nodes:
-        print(node.nombre)
-
-    print("Cabs data")
-    for cab in cabs:
-        print(cab.placa)
+    routes = Routes()
+    routes.bestFirstSearch(nodes, "Nodo1", "Nodo8")
 
 
 if __name__ == "__main__":
