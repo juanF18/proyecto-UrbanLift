32561e4480dcdfdea390b65727bbaf056637cd94
Juan Felipe Cortes <juanfelipecortescastrillon@gmail.com>
2024-04-03 23:26:48 -0500

    adds fuel-efficient a* algorithm

--- main.py
+++ main.py
@@ -17,25 +17,13 @@ def main():
     cabs = Cab.load_cabs(data)
 
     # call bfs
-    routes = Routes()
-    inicio = "Nodo7"
-    fin = "Nodo9"
-    path = routes.bestFirstSearch(nodes, inicio, fin)
-
-    nodo = fin
-    camino = [nodo]
-    while nodo != (inicio):
-        nodo = path[nodo]
-        camino.append(nodo)
-
-    camino.reverse()
-    print(camino)
+    routes_bfs = Routes()
 
     # functions of node routes
     routes = Node_paths()
 
     print("Nodes data")
-    for k, v in nodes:
+    for k, v in nodes.items():
         print(v.nombre)
 
     print("Cabs data")
@@ -50,6 +38,25 @@ def main():
     print("End Node: ", end)
     path = routes.astar_shortest_route(nodes, "Nodo1", "Nodo5")
     print("Path: ", path)
+    print("-" * 50)
+    print("Best firs search")
+    inicio = "Nodo7"
+    fin = "Nodo9"
+    path = routes_bfs.bestFirstSearch(nodes, inicio, fin)
+    nodo = fin
+    camino = [nodo]
+    while nodo != (inicio):
+        nodo = path[nodo]
+        camino.append(nodo)
+
+    camino.reverse()
+    print("Path: ", camino)
+    print("-" * 50)
+    print("Route a* with lower fuel consumption")
+    Node.resetting_euristics_values(nodes)
+    path, total_fuel = routes.astar_with_gas(nodes, "Nodo1", "Nodo5", 15)
+    print("Path: ", path)
+    print("Total fuel expense was: ", total_fuel)
 
 
 if __name__ == "__main__":
