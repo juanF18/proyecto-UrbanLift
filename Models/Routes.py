f66702d0562490cb71a67d47f1150b7df3f39fbe
Camilo <camian6@outlook.com>
2024-04-06 21:08:24 -0500

    Method to calculate cheapest route
    method was added, code should be modified.
    Also was added their call into main file

--- Models/Routes.py
+++ Models/Routes.py
@@ -51,3 +51,48 @@ class Routes:
             manhattan += newNode.get_semaforo()["duration"]
 
         return manhattan
+
+    """ 
+        method to calculate shortest route (less traffic lights)
+        Args:
+            nodeList: Dict
+            start: str
+            finish: str
+    """
+
+    def hillClimbing(self, L, inicio, final, iteraciones_max=100):
+        pos_actual = inicio
+        valor_actual = self.heuristicHill(L, pos_actual, final)
+
+        for _ in range(iteraciones_max):
+            candidatos = [
+                (pos_actual[0] + 1, pos_actual[1]),
+                (pos_actual[0] - 1, pos_actual[1]),
+                (pos_actual[0], pos_actual[1] + 1),
+                (pos_actual[0], pos_actual[1] - 1),
+            ]
+
+            candidatos = []
+
+            if not candidatos:
+                break
+
+            candidato = max(candidatos, key=lambda n: self.heuristicHill(L, n, final))
+            valor_candidato = self.heuristicHill(L, candidato, final)
+
+            if valor_candidato > valor_actual:
+                L[pos_actual[0]][pos_actual[1]] = 2
+                pos_actual = candidato
+                valor_actual = valor_candidato
+
+            if pos_actual == final:
+                L[pos_actual[0]][pos_actual[1]] = 2
+                print("¡Objetivo alcanzado!")
+                break
+
+        return L
+
+    def heuristicHill(self, L, pos_actual, final):
+        return -(
+            ((pos_actual[0] - final[0]) ** 2 + (pos_actual[1] - final[1]) ** 2) ** 0.5
+        )
