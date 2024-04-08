4d2b5db54af7486a46e257c9fc1b0c5a150502d7
Camilo <camian6@outlook.com>
2024-04-07 19:11:25 -0500

    Shortest route added
    Method to calculate shortest route from taxi to user, not finished

--- Models/Routes.py
+++ Models/Routes.py
@@ -1,4 +1,5 @@
 from queue import PriorityQueue
+import heapq
 
 
 class Routes:
@@ -96,3 +97,49 @@ class Routes:
             manhattan += actualNode.get_semaforo()["duration"]
 
         return manhattan
+
+    def astarShortestRoute(self, nodes_dict, start_name, end_name):
+        open_list = []
+        closed_list = set()
+
+        start_node = nodes_dict[start_name]
+        end_node = nodes_dict[end_name]
+
+        heapq.heappush(open_list, (start_node.nombre, start_node))
+
+        while open_list:
+            _, current_node = heapq.heappop(open_list)
+            closed_list.add(current_node.nombre)
+
+            if current_node == end_node:
+                path = []
+                while current_node:
+                    path.append(current_node.nombre)
+                    current_node = current_node.parent
+                return path[::-1]
+
+            for neighbor in current_node.conexiones:
+                if neighbor.nombre in closed_list:
+                    continue
+
+                tentative_g = current_node.g + 1
+                tentative_f = tentative_g + self.__calculate_heuristic(
+                    neighbor, end_node
+                )
+
+                in_open_list = False
+                for item in open_list:
+                    _, open_node = item
+                    if neighbor.nombre == open_node.nombre:
+                        in_open_list = True
+                        if tentative_g >= neighbor.g:
+                            break
+                if not in_open_list or tentative_g < neighbor.g:
+                    neighbor.g = tentative_g
+                    neighbor.h = self.__calculate_heuristic(neighbor, end_node)
+                    neighbor.f = tentative_f
+                    neighbor.parent = current_node
+                    if not in_open_list:
+                        heapq.heappush(open_list, (neighbor.nombre, neighbor))
+
+        return None
